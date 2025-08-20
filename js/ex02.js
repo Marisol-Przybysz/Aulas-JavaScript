173fb07096c84741d0c4bc846df9c8584f68c4c3
Marisol-Przybysz <marisol.przybysz.ifpr.2023@gmail.com>
2025-08-19 21:08:08 -0300

    Add files via upload

---
+++ js/ex02.js
@@ -0,0 +1,26 @@
+/*Variáveis em JS*/
+var frase
+frase = "Aqui temos uma String"
+alert(frase)
+
+/*Fazendo entrada de dados para uma variável*/
+var nome 
+nome = prompt("Digite o seu nome:")
+alert("Bem vindo "+nome)
+
+var idade
+idade=prompt("Digite a sua idade:")
+alert("Você tem "+idade+" anos")
+
+/*Operações Aritméticas com JS*/
+var n1, n2, r
+n1=14
+n2=20
+r=n1+n2
+alert("O resultado da soma entre "+n1+" + "+n2+" = "+r)
+
+/*Considere que os valores n1 e n2 sejam digitados pelo usuário*/
+n1= parseInt(prompt("Digite a idade 1"))
+n2= parseInt(prompt("Digite a idade 2"))
+r = n1 + n2
+alert("O resultado da soma entre "+n1+" + "+n2+" = "+r)
